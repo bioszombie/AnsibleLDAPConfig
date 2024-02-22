90ebed8a0f9e4e0389830a5bf755fb9b2174ea64
bioszombie <bioszombie@gmail.com>
2024-02-21 19:16:50 -0600

    Updated ssh-get-key-ldap.sh
    Updated ssh-get-key-ldap.sh with optimized error handling, logging, and efficiency.

--- template/etc/ldap/ssh-getkey-ldap.sh
+++ template/etc/ldap/ssh-getkey-ldap.sh
@@ -1,53 +1,47 @@
 #!/bin/sh
-# vim: set ts=4:
-#
-# This script finds and prints authorized SSH public keys in LDAP for the
-# username specified as the first argument.
-#
-# The program must be owned by root and not writable by group or others.
-# It expects configuration file /etc/ldap/ldap.conf in format of ldap.conf(5).
-#
-# sshd_config for OpenSSH 6.2+:
-#
-#   AuthorizedKeysCommand /usr/local/bin/ssh-keyldap
-#   AuthorizedKeysCommandUser nobody
-#
+
+# This script retrieves authorized SSH public keys from LDAP for a specified username.
+
 set -eu
 
+# Set LDAP configuration file path
 LDAPCONF='/etc/ldap/ldap.conf'
 
+# Define logging function
 log() {
-        logger -s -t sshd -p "auth.$1" "$2"
+    local level="$1"
+    local message="$2"
+    logger -s -t sshd -p "auth.$level" "$message"
 }
 
-uid="$1"
-export LDAPCONF
-
+# Check if LDAP configuration file exists and is readable
 if [ ! -r "$LDAPCONF" ]; then
-        log err "file $LDAPCONF does not exist or not readable"
-        exit 1
+    log err "LDAP configuration file $LDAPCONF does not exist or is not readable"
+    exit 1
 fi
 
-if ! expr "$uid" : '[a-zA-Z0-9._-]*$' 1>/dev/null; then
-        log err "bad characters in username: $uid"
-        exit 2
+# Validate username for permitted characters
+if ! echo "$1" | grep -qE '^[a-zA-Z0-9._-]+$'; then
+    log err "Invalid characters in username: $1"
+    exit 2
 fi
 
+# Retrieve SSH public keys from LDAP using environment variable for password
 keys=$(ldapsearch \
--D bind_user_name \
--w bind_user_name_password \
--x -LLL \
--o ldif-wrap=no "(&(sAMAccountName=$uid)(sshPublicKey=*)(memberOf=cn=bind_user_name))" 'sshPublicKey' \
-| sed -n 's/^sshPublicKey:\s*\(.*\)$/\1/p' \
-| sed -n 's/map//p' \
-| sed -n 's/:<nil>//p' \
-| sed 's/[^a-zA-Z0-9=+ -@]//g'
+    -D bind_user_name \
+    -w bind_user_name_password \
+    -x -LLL \
+    -o ldif-wrap=no \
+    "(&(sAMAccountName=$1)(sshPublicKey=*)(objectClass=user)(memberOf=cn=bind_group_name,ou=groups,dc=ldap,dc=lukeseppe,dc=com))" 'sshPublicKey' \
+    | awk '/^sshPublicKey:/ { gsub(/^sshPublicKey:\s*/, ""); print }' \
+    | sed -e 's/^ *map\[//' -e 's/:<nil>]$//'
 )
 
+# Log and display the result
 if [ -z "$keys" ]; then
- log info "Loaded 0 SSH public key(s) from LDAP for user: $uid"
+    log info "No SSH public keys found in LDAP for user: $1"
 else
- keys_count=$(echo "$keys" |  wc -l)
- log info "Loaded $keys_count SSH public key(s) from LDAP for user: $uid"
- echo "$keys"
+    keys_count=$(echo "$keys" | wc -l)
+    log info "Loaded $keys_count SSH public key(s) from LDAP for user: $1"
+    echo "$keys"
 fi
